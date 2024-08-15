77eda37fbbe34b12fceaf920266011afcc20bbd1
Muhammad Faiq khan <155578969+faiqkhan125@users.noreply.github.com>
2024-08-15 23:56:18 +0500

    FIREBASE HECHAZONE COMMIT
    FIREBASE HECHAZONE COMMIT

---
+++ main.js
@@ -0,0 +1,32 @@
+import {
+    onAuthStateChanged,
+    signOut,
+  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  
+  
+  import { auth } from "./config.js";
+  
+  const logOutBtn = document.querySelector("#log-out-btn");
+  
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid);
+    } else {
+     window.location="login.html"
+    }
+  });
+  
+  
+  logOutBtn.addEventListener('click' , ()=>{
+  signOut(auth)
+    .then(() => {
+      alert('You are Log Out sucessfully!')
+      window.location="login.html"
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+  
+  })
